1a9fa8667b0302f01976efe1cdd2664f3b85993d
aryumka <yooooar@gmail.com>
2024-04-11 00:02:35 +0900

    Make queue durable and message persistent

--- src/main/kotlin/messaging/NewTask.kt
+++ src/main/kotlin/messaging/NewTask.kt
@@ -1,6 +1,7 @@
 package messaging
 
 import com.rabbitmq.client.ConnectionFactory
+import com.rabbitmq.client.MessageProperties
 
 class Sender {
   private val QUEUE_NAME = "task_queue"
@@ -11,11 +12,21 @@ class Sender {
 
     factory.newConnection().use { connection ->
       connection.createChannel().use { channel ->
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null)
+        val durable = true
+        // durable을 true로 설정하면 RabbitMQ가 재시작되어도 큐가 유지된다.
+        channel.queueDeclare(QUEUE_NAME, durable, false, false, null)
         // 프로그램 실행 인자를 메시지로 보낸다.
         val message = argv.joinToString(" ")
 
-        channel.basicPublish("", QUEUE_NAME, null, message.toByteArray())
+        channel.basicPublish(
+          "",
+          QUEUE_NAME,
+          // 메시지를 디스크에 영속화한다.
+          // 하지만 메시지 무손실을 보장하지 않는다. RabbitMQ가 메시지를 디스크에 저장하기 전에 죽으면 메시지는 손실될 수 있다.
+          // 메시지를 무손실로 보장하려면 publisher confirms를 사용해야 한다. https://www.rabbitmq.com/docs/confirms
+          MessageProperties.PERSISTENT_TEXT_PLAIN,
+          message.toByteArray()
+        )
         println(" [x] Sent '$message'")
       }
     }
--- src/main/kotlin/messaging/Worker.kt
+++ src/main/kotlin/messaging/Worker.kt
@@ -14,8 +14,12 @@ class Receiver {
     val connection = factory.newConnection()
     val channel = connection.createChannel()
 
-    channel.queueDeclare(QUEUE_NAME, false, false, false, null)
+    val durable = true
+    channel.queueDeclare(QUEUE_NAME, durable, false, false, null)
     println(" [*] Waiting for messages. To exit press Ctrl+C")
+
+    // 한 번에 하나의 메시지만 받도록 한다.
+    channel.basicQos(1);
 
     val deliverCallback = DeliverCallback { _, delivery ->
       val message = String(delivery.body, charset("UTF-8"))
@@ -26,10 +30,19 @@ class Receiver {
         e.printStackTrace()
       } finally {
         println(" [x] Done")
+        // 만약 basicAck을 호출하지 않으면 unAck 상태의 메시지들은 재전송되지 못하고 메모리 이슈가 발생한다.
+        // 이러한 이슈를 디버깅하기 위해서는
+        // sudo rabbitmqctl list_queues name messages_ready messages_unacknowledged
+        // rabbitmqctl.bat list_queues name messages_ready messages_unacknowledged -- windows
+        // 또는 RabbitMQ Management UI에서 확인할 수 있다.
+        channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
       }
+      // autoAck를 true로 설정하면 RabbitMQ에 메시지를 전달한 후 바로 삭제한다. 디폴트는 true이다.
+      // autoAck를 false로 설정하면 컨슈머가 메시지 처리를 완료했을 때 메시지가 삭제되고 unAck상태의 메시지들은 재전송된다.
+      // 메시지 처리가 완료되면 RabbitMQ에게 메시지 처리가 완료되었음을 알린다.
     }
-    val autoAck = true // autoAck를 true로 설정하면 RabbitMQ에 메시지를 전달한 후 바로 삭제한다. 디폴트는 true이다.
-    channel.basicConsume(QUEUE_NAME, true, deliverCallback) { _: String? -> }
+    val autoAck = false
+    channel.basicConsume(QUEUE_NAME, autoAck, deliverCallback) { consumerTag -> }
   }
 
   //시간이 걸리는 작업 수행을 모방하기 위해 메시지의 각 . 개수마다 1초를 기다린다.
